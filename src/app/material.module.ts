3655c955cf048e9a3349117f2139759d61a4400b
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2020-07-19 18:34:26 +0300

    changes before updates

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: [],
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule {}
